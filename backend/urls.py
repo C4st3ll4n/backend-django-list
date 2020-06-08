859b7ceea01805b99f8c1e914439d67f5c9489c5
Pedro Henrique <p13dr0h@gmail.com>
2020-06-08 14:15:38 -0300

    token ok

--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from .views import GroupViewSet, UserViewSet
+from core.views import ListViewSet, ItemViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'list', ListViewSet)
+router.register(r'item', ItemViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
